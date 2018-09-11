da4f821c632bd9ce4e22ceddd8e3cf545459647c
serubi <serubi@live.com>
2018-09-11 11:16:27 +0200

    Tilføjet unit test

--- PrimeService/PrimeService.cs
+++ PrimeService/PrimeService.cs
@@ -6,6 +6,10 @@ namespace PrimeService
     {
         public bool IsPrime(int candidate)
         {
+            if (candidate == 1)
+            {
+                return false;
+            }
             throw new NotImplementedException("Please create a test first");
         }
     }
